5a8e5ae3019c75cf9a3fc57b2b7633fe88067cb7
lukasalexanderconrads <Abba1024-github>
2023-07-17 18:37:53 +0200

    added option for multiple samples per data point

--- scripts/generate_latent_trajectory.py
+++ scripts/generate_latent_trajectory.py
@@ -21,9 +21,11 @@ def main(config_path: Path):
 
         model_config = read_yaml(os.path.join(model_dir, next(os.scandir(model_dir)), 'config.yaml'))
 
+        batch_size = config['args']['batch_size']
+        time_point_batch_size = config['args']['time_point_batch_size']
         model_config['trainer']['args']['no_training'] = True
         model_config['loader']['args']['shuffle'] = False
-        model_config['loader']['args']['batch_size'] = config['args']['batch_size']
+        model_config['loader']['args']['batch_size'] = batch_size
         model_config['device'] = config['device']
 
         trainer = get_trainer(model_config)
@@ -31,49 +33,52 @@ def main(config_path: Path):
         model = trainer.model
 
         save_dir = config['args']['path']
-        num_steps = config['args'].get('num_steps', torch.inf)
-        cutoff_metric = config['args'].get('cutoff_metric', 'loss')
-        cutoff_var = config['args'].get('cutoff_var', 0)
-        cutoff_n = config['args'].get('cutoff_n', 1)
-        cutoff_value = config['args'].get('cutoff_value', torch.inf)
+        max_n_time_points = config['args'].get('max_n_time_points', torch.inf)
 
-        trajectories_per_model = config['args'].get('trajectories_per_model')
-
+        n_trajectories_per_data = config['args'].get('n_trajectories_per_data', 1)
+        n_trajectories_per_model = config['args'].get('n_trajectories_per_model', None)
+        if n_trajectories_per_model is None:
+            n_trajectories_per_model = len(data_loader.train.dataset)
         cov_factor = config['args'].get('cov_factor', 1)
 
         # create save_dir if not exists
         model_name = os.path.join(*(model_dir.split('/')[3:]))
         Path(os.path.join(save_dir, model_name)).mkdir(parents=True, exist_ok=True)
 
-        latent_traj_ensemble = []
+        timestamp_list = [_ for _ in os.scandir(model_dir)]
+        n_timestamps = len(timestamp_list)
 
-        timestamp_list = os.scandir(model_dir)
-        n_timestamps = len([_ for _ in os.scandir(model_dir)])
-        for timestamp in tqdm(timestamp_list, total=n_timestamps):
+        ### set up memory map ###
+        # get trajectory length
+        with open(os.path.join(model_dir, timestamp_list[0], 'checkpoint_names.txt')) as f:
+            checkpoint_names = f.readlines()
 
-            latent_traj = []
+        n_time_points = min(len(checkpoint_names), max_n_time_points)
+        assert n_time_points % time_point_batch_size == 0, \
+            f'time point batch size {time_point_batch_size} not divisible by number of time points {n_time_points}'
+        n_trajects = n_timestamps * n_trajectories_per_model * n_trajectories_per_data
+        data_dim = data_loader.train.dataset.latent_shape
+        traject_shape = (n_trajects, n_time_points, data_dim)
+
+        traject_file = os.path.join(save_dir, model_name, 'latent.dat')
+        file_map = np.memmap(traject_file, dtype='float32', mode='w+', shape=traject_shape)
+
+        # FOR EACH MODEL
+        for model_number, timestamp in tqdm(enumerate(timestamp_list), total=n_timestamps):
+
             lr_traj = []
             loss_traj = []
             mi_traj = []
 
-            error_occured = False
             # checkpoint file names are in checkpoint_names.txt
             with open(os.path.join(model_dir, timestamp, 'checkpoint_names.txt')) as f:
                 checkpoint_names = f.readlines()
 
-            # for each checkpoint
+            # FOR EACH TIME POINT
+            latent_time_step_batch = []
             for step, checkpoint_name in tqdm(enumerate(checkpoint_names), total=len(checkpoint_names), leave=False):
-                if step >= num_steps:
+                if step >= max_n_time_points:
                     break
-
-                # cut off if variance of last cutoff_n values of cutoff_mmetric is less than cutoff_var
-                cutoff_metric_list = mi_traj if cutoff_metric == 'mi' else loss_traj
-                #var = torch.var(torch.tensor(cutoff_metric_list[-min(cutoff_n, len(mi_traj)):]))
-                # if var < cutoff_var and len(cutoff_metric_list) > cutoff_n:
-                #     break
-                if cutoff_value < torch.inf:
-                    if torch.mean(torch.tensor(cutoff_metric_list[-min(cutoff_n, len(mi_traj)):])) > cutoff_value and len(cutoff_metric_list) > cutoff_n:
-                        break
 
                 # load the model from the checkpoint
                 model.load_state_dict(torch.load(os.path.join(model_dir, timestamp, checkpoint_name[:-1])))
@@ -81,50 +86,56 @@ def main(config_path: Path):
                 # load the data
                 latent = []
                 loss = []
-                for i, minibatch in enumerate(data_loader.train):
+                # FOR EACH MINIBATCH
+                for batch_number, minibatch in enumerate(data_loader.train):
                     data = minibatch['data'].to(model.device).float()
 
-                    # save latent code
-                    try:
-                        latent_batch, mi, loss_batch = model.get_latent_mi_loss(data, cov_factor=cov_factor)
-                    except:
-                        print(f'could not use batch {i} of step {checkpoint_name}')
+                    data = data.repeat_interleave(n_trajectories_per_data, dim=0)
 
-                    latent.append(latent_batch)
+                    # save latent code
+                    latent_batch, mi, loss_batch = model.get_latent_mi_loss(data, cov_factor=cov_factor)
+                    latent.append(latent_batch.detach())
                     # compute loss
                     loss.append(loss_batch)
 
-                    if len(latent) * latent_batch.size(0) >= trajectories_per_model:
-                        break
+                    # if (batch_number + 1) * latent_batch.size(0) * n_trajectories_per_data >= n_trajectories_per_model:
+                    #     print()
+                    #     break
+
+                latent_time_step_batch.append(torch.cat(latent, dim=0))
+
+                if len(latent_time_step_batch) == time_point_batch_size:
+                    print(n_trajectories_per_model, n_trajectories_per_data)
+                    batch_start_idx = model_number * n_trajectories_per_model * n_trajectories_per_data
+                    batch_end_idx = (model_number + 1) * n_trajectories_per_model * n_trajectories_per_data
+                    time_step_start_idx = step - time_point_batch_size + 1
+                    time_step_end_idx = step + 1
+
+                    latent_time_step_batch = torch.stack(latent_time_step_batch, dim=1)
+                    file_map[batch_start_idx:batch_end_idx, time_step_start_idx:time_step_end_idx] = latent_time_step_batch.cpu().numpy().copy()
+
+                    latent_time_step_batch = []
 
                 mi_traj.append(mi)
 
                 # lr
                 lr_traj.append(trainer.optimizer.param_groups[0]['lr'])
-
-
-                latent = torch.cat(latent, dim=0)
-                latent_traj.append(latent.detach().cpu())
-
+                # loss
                 loss = torch.mean(torch.tensor(loss))
                 loss_traj.append(loss)
 
-            latent_traj = torch.stack(latent_traj, dim=1)
+
+
             lr_traj = torch.tensor(lr_traj)
             loss_traj = torch.tensor(loss_traj)
 
-            latent_traj_ensemble.append(latent_traj)
 
-        latent_traj = torch.cat(latent_traj_ensemble, dim=0)
 
-        print(f'saving latent codes of shape {latent_traj.size()} to', os.path.join(save_dir, model_name, 'latent.dat'))
+        print(f'saving latent codes of shape {traject_shape} to', os.path.join(save_dir, model_name, 'latent.dat'))
         print(f'saving learning rates of shape {lr_traj.size()} to', os.path.join(save_dir, model_name, 'learn_rate.pt'))
         print(f'saving loss of shape {loss_traj.size()} to', os.path.join(save_dir, model_name, 'loss.pt'))
 
-        traject_shape = latent_traj.size()
-        traject_file = os.path.join(save_dir, model_name, 'latent.dat')
-        file_map = np.memmap(traject_file, dtype='float32', mode='w+', shape=traject_shape)
-        file_map[:] = latent_traj.detach().cpu().numpy()
+        # close latent file map
         file_map.flush()
 
         np.save(os.path.join(save_dir, model_name, 'data_shape.npy'), np.array(traject_shape))
